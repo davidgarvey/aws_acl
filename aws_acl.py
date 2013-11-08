e4bf9a23484889588f935528dc9b352bfc791a46
davidgarvey <david.garvey@gmail.com>
2013-11-08 15:10:17 -0800

    Update aws_acl.py

--- aws_acl.py
+++ aws_acl.py
@@ -1,6 +1,6 @@
 import cherrypy
 cherrypy.config.update({'server.socket_port': 9999})
-cherrypy.server.socket_host = '66.116.97.86'
+cherrypy.server.socket_host = 'yourip'
 #cherrypy.engine.restart()
 from os import path
 import os
